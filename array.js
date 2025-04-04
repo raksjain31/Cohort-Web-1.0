1fa798038d054937f344448832177a0cbc9049f9
Rakshit Rathod <rakshitjain610@gmail.com>
2025-04-04 07:20:28 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,10 @@
+let fruits = ["apple","cherry","banana"];
+let intFruits = new Array ("Kiwi","Orange","Mango");
+
+fruits.push("Watermelon");
+ console.log(fruits);
+ 
+ fruits.unshift("NewEntry");
+ console.log(fruits.length);
+ console.log(fruits);
+
